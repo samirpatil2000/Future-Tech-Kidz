7d87bb8fb901aa78fe267d8fbe3e083255042a99
Samir Patil <samirspatil742099@gmail.com>
2022-11-03 02:09:50 +0530

    [NEW] Added html and css templates

--- course/urls.py
+++ course/urls.py
@@ -3,4 +3,6 @@ from django import views
 from . import views
 
 urlpatterns = [
+    path('', views.home),
+    path('login/', views.login)
 ]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, template_name='course/index.html')
+
+def login(request):
+    return render(request, template_name='course/form.html')
