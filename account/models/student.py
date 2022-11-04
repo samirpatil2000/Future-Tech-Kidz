7964502730567f5760613435cfadf7d73f988d49
Samir Patil <samirspatil742099@gmail.com>
2022-11-05 01:33:13 +0530

    [NEW] New Models Added

--- account/models/student.py
+++ account/models/student.py
@@ -5,3 +5,8 @@ class Student(Account):
 
     school_name = models.CharField(max_length=30)
     franchisee_name = models.ForeignKey('course.Franchisee', on_delete=models.SET_NULL, blank=True, null=True)
+
+    @property
+    def total_fees(self):
+
+        return
