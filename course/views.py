5ef9efadc7c7a12df2adfc2cf95161063053f2b3
Samir Patil <samirspatil742099@gmail.com>
2022-11-23 02:33:06 +0530

    [New] Added Franchisee and Course Filter

--- course/views.py
+++ course/views.py
@@ -8,13 +8,29 @@ from account.models import Account, Student
 def home(request):
     return render(request, template_name='course/index.html')
 
-
 def index(request):
     return render(request, template_name='course/table.html')
 
-
-def student_list(request):
+def get_students(request, **kwargs):
+    title = kwargs.get('title') and kwargs.pop('title')
     context = {
-        "enroll_students": Enrollment.objects.all()
+        "title": title,
+        "enroll_students": Enrollment.objects.filter(**kwargs)
     }
     return render(request, template_name='course/student_list.html', context=context)
+
+def student_list(request):
+    return get_students(request)
+
+def student_by_franchise(request, franchise_id):
+    return get_students(request, student__franchisee_name_id=franchise_id, title="Students By Franchise")
+
+def student_by_course(request, course_id):
+    return get_students(request, course_id=course_id, title="Students By Course")
+
+
+def add_student(request):
+    context = {
+
+    }
+    return render(request, template_name='course', context=context)
