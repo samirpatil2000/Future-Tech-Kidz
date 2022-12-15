d7d868bc8d68ef2aed3e4123cd30cc349b4aef1e
Samir Patil <samirspatil742099@gmail.com>
2022-12-15 23:13:36 +0530

    [UPDATE] remove required pn reference field in transactions table

--- course/models/transactions.py
+++ course/models/transactions.py
@@ -6,7 +6,7 @@ class Transaction(models.Model):
     enrollment = models.ForeignKey("course.enrollment", on_delete=models.SET_NULL, blank=True, null=True)
     paid_at = models.DateTimeField(verbose_name='date of payment', auto_now_add=True)
     created_by = models.ForeignKey("account.Account", on_delete=models.SET_NULL, blank=True, null=True)
-    reference = models.TextField()
+    reference = models.TextField(blank=True, null=True)
     amount = models.IntegerField()
     franchisee = models.ForeignKey("course.Franchisee", on_delete=models.SET_NULL, blank=True, null=True)
 
